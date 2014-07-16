d471f3d2fd98d3d223c187d98b01d34773d16b9b
Charles Kalmanek <ckalmanek@gmail.com>
2014-07-16 10:45:07 -0400

    Added api_root

--- notesapp/urls.py
+++ notesapp/urls.py
@@ -10,10 +10,11 @@ urlpatterns = patterns('',
     # url(r'^$', 'notes.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^notesapp/$', views.NoteList.as_view()),
+    url(r'^notesapp/$', views.NoteList.as_view(), name='note-list'),
     url(r'^notesapp/(?P<pk>[0-9]+)/$', views.NoteDetail.as_view()),
-    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/$', views.UserList.as_view(), name='user-list'),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^$', 'notesapp.views.api_root'),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- notesapp/views.py
+++ notesapp/views.py
@@ -1,6 +1,9 @@
 from rest_framework import status
+from rest_framework import renderers
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from rest_framework import generics
 from rest_framework import permissions
 from notesapp.models import Note
@@ -36,13 +39,20 @@ class UserList(generics.ListAPIView):
     # List all users
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly)
+    # permission_classes = (permissions.IsAuthenticatedOrReadOnly)
 
 
 class UserDetail(generics.RetrieveAPIView):
     # Retrieve a user
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
-	pemission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-	
+	# pemission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+@api_view(('GET',))
+def api_root(request, format=None):
+	return Response({
+		'users': reverse('user-list', request=request, format=format),
+		'notesapp': reverse('note-list', request=request, format=format)
+	})
+
 
